878577295c81f02483959614fdddce4ab05766d7
DavenMauff <dsmauff@gmail.com>
2019-09-02 15:10:11 +0200

    pushy

---
+++ movies/Daven.R
@@ -0,0 +1,11 @@
+pacman::p_load(rjson, jsonlite, DT,  RJSONIO)
+
+metadata = read.csv("../data-raw/movies_metadata.csv")
+
+head(metadata$belongs_to_collection)
+
+metadata$belongs_to_collection <- gsub("'", '"', metadata$belongs_to_collection)
+head(metadata$belongs_to_collection)
+
+belongs <- fromJSON(metadata$belongs_to_collection)
+head(belongs)
